f2e345bbce523d7b34b944c58d835cf528416116
Ryan Culpepper <ryanc@racket-lang.org>
2015-10-25 04:15:36 -0400

    ... initialize managers

--- init.rkt
+++ init.rkt
@@ -1,10 +1,11 @@
 #lang racket/base
 (require (rename-in racket/match [match-define defmatch])
          racket/cmdline
+         racket/string
          "private/net.rkt"
          "private/db.rkt")
 
-(define (init [commit "master"])
+(define (init-branch-day [commit "master"])
   (define catalog
     (get/url (get-catalog-url commit)
              #:handle read))
@@ -20,8 +21,6 @@
 
 (define (get-catalog-url [commit "master"])
   (format "https://raw.githubusercontent.com/racket/release-catalog/~a/release-catalog/pkgs-all" commit))
-
-;; ============================================================
 
 (define github-rx
   (regexp
@@ -43,16 +42,44 @@
 
 ;; ============================================================
 
+;; file contains: ((manager owner/repo ...) ...)
+(define (init-managers managers-file)
+  (define all-repos (call-with-input-file* "repos.rktd" read))
+  (define assigned-repos (make-hash))
+  (define managers (call-with-input-file* managers-file read))
+  (for ([entry managers])
+    (define manager (symbol->string (car entry)))
+    (define repos
+      (for/list ([o/r (cdr entry)])
+        (unless (member o/r all-repos)
+          (eprintf "** bad repo: ~s\n" o/r))
+        (hash-set! assigned-repos o/r manager)
+        (string-split (symbol->string o/r) "/")))
+    ;; (eprintf "adding ~s managing ~s\n" manager repos)
+    (db:create-manager manager repos))
+  (for ([repo all-repos]
+        #:when (not (hash-ref assigned-repos repo #f)))
+    (eprintf "** unassigned repo: ~s\n" repo)))
+
+;; ============================================================
+
 (module+ main
   (require racket/cmdline)
 
   (define DB-FILE (the-db-file))
-  (define COMMIT "master")
+  (define COMMIT #f)
+  (define MANAGERS-FILE #f)
 
   (command-line
    #:once-each
    [("--db-file") db-file "Use/create database at db-file" (set! DB-FILE db-file)]
-   [("--commit") commit "Use commit of release catalog" (set! COMMIT commit)]
+   [("--commit") commit "Use release catalog at <commit> for branch-day" (set! COMMIT commit)]
+   [("--managers") managers-file "Use file to initialize managers" (set! MANAGERS-FILE managers-file)]
    #:args ()
    (parameterize ((the-db-file DB-FILE))
-     (init COMMIT))))
+     (when COMMIT
+       (printf "Initializing branch-day table\n")
+       (init-branch-day COMMIT))
+     (when MANAGERS-FILE
+       (printf "Initializing managers table\n")
+       (init-managers MANAGERS-FILE)))))
